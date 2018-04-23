ac313fe33c34c61976ea31ef1a2ccef81249d14e
Morell <jrdmorell@gmail.com>
2018-04-23 13:23:23 +0200

    Open on new tab

--- plugin.rb
+++ plugin.rb
@@ -19,6 +19,6 @@ class Onebox::Engine::GameBananaOnebox
 	end
 	
 	def to_html
-		"<a href=\"https://gamebanana.com/#{category}/#{id}\"><img alt=\"Share banner\" src=\"https://gamebanana.com/#{category}/embeddables/#{id}?type=large\"/></a>"
+		"<a href=\"https://gamebanana.com/#{category}/#{id}\" target=\"_blank\"><img alt=\"Share banner\" src=\"https://gamebanana.com/#{category}/embeddables/#{id}?type=large\"/></a>"
 	end
 end
